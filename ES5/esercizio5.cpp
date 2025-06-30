f7afa7194a57923e46d4262c4eda7dbf18039636
catezac <catezac@CateZaccone.localdomain>
2025-06-30 19:13:23 +0200

    es5 completato

--- ES5/esercizio5.cpp
+++ ES5/esercizio5.cpp
@@ -18,22 +18,24 @@ int main() {
     string method = SetMethod("input.dat", "method");
     double passo_gs = SetParameter("input.dat", "passo_gs");
     double passo_2p = SetParameter("input.dat", "passo_2p");
+    int x_0 = SetParameter("input.dat", "x_0");
     double a_0 = 1;
     double L = M/N;
 
     // funzione d'onda GS
 
     vector <Posizione> x_GS(M);
-    x_GS[0].setX(0);
+    x_GS[0].setX(x_0);
     x_GS[0].setY(0);
     x_GS[0].setZ(0);
     double acceptance_gs = 0;
     double r_gs = 0;
     
     // equilibrazione del sistema 
-    equilibration(rnd, M_equil, x_GS, a_0, ground_state, method, passo_gs, acceptance_gs);
-    cout << "Accettanza GS con passo " << passo_gs << " : " << acceptance_gs << endl;
-
+    if(x_0 == 0){
+        equilibration(rnd, M_equil, x_GS, a_0, ground_state, method, passo_gs, acceptance_gs);
+        cout << "Accettanza GS con passo " << passo_gs << " : " << acceptance_gs << endl;
+    }
     // ora faccio il ciclo di Metropolis e la media a blocchi
     vector<double> ave_GS(M); 
     vector<double> av2_GS(M);
@@ -52,21 +54,22 @@ int main() {
     }
 
     cout << "Accettanza GS, con probabilità di transizione " << method << ": " << acceptance_gs/M << endl;
-    WriteToFile_pos("OUTPUT/prob_GS.dat", M, x_GS);
+    WriteToFile_pos("OUTPUT/prob_GS_" + to_string(x_0) + ".dat", M, x_GS);
     auto result_gs_unif = sum_prog(ave_GS, av2_GS, N);
-    WriteToFile_r("OUTPUT/r_medio_GS_" + method + ".dat", N, result_gs_unif.first, ave_GS, result_gs_unif.second);
+    WriteToFile_r("OUTPUT/r_medio_GS_" +  method + "_" + to_string(x_0) + ".dat", N, result_gs_unif.first, ave_GS, result_gs_unif.second);
 
     // funzione d'onda 2p
 
     vector <Posizione> x_2p(M);
-    x_2p[0].setX(0);
+    x_2p[0].setX(x_0);
     x_2p[0].setY(0);
     x_2p[0].setZ(0);
     double acceptance_2p = 0;
     double r_2p = 0;
-
-    equilibration(rnd, M_equil, x_2p, a_0, state_2p, method, passo_2p, acceptance_2p);
-    cout << "Accettanza 2p con passo " << passo_2p << " : " << acceptance_2p << endl;
+    if(x_0==0){
+        equilibration(rnd, M_equil, x_2p, a_0, state_2p, method, passo_2p, acceptance_2p);
+        cout << "Accettanza 2p con passo " << passo_2p << " : " << acceptance_2p << endl;
+    }
     vector<double> ave_2p(M); 
     vector<double> av2_2p(M);
 
@@ -85,9 +88,9 @@ int main() {
     }
 
     cout << "Accettanza 2p, con probabilità di transizione " << method <<  ": " << acceptance_2p/M << endl;
-    WriteToFile_pos("OUTPUT/prob_2p.dat", M, x_2p);     
+    WriteToFile_pos("OUTPUT/prob_2p_" + to_string(x_0) + ".dat", M, x_2p);     
     auto result_2p = sum_prog(ave_2p, av2_2p, N);
-    WriteToFile_r("OUTPUT/r_medio_2p_" + method + ".dat", N, result_2p.first, ave_2p, result_2p.second);
+    WriteToFile_r("OUTPUT/r_medio_2p_" + method + "_" + to_string(x_0) + ".dat", N, result_2p.first, ave_2p, result_2p.second);
 
     return 0;
 }
